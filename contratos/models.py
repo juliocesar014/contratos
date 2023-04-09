8724085b67f2f4925977d4ade80fb6d9b6b0ef80
juliocesar014 <juliocesar014@live.com>
2023-04-08 21:39:30 -0300

    fix: Add contratos in file static

--- contratos/models.py
+++ contratos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Contrato(models.Model):
     nome = models.CharField(max_length=100)
-    arquivo = models.FileField(upload_to='contratos/')
+    arquivo = models.FileField(upload_to='contratos/pastacontratos/')
     usuario = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     data = models.DateTimeField(auto_now_add=True)
 

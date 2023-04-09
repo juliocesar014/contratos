214a40904a253d4c89feb1b43c9108d03d81940a
juliocesar014 <juliocesar014@live.com>
2023-04-08 21:46:58 -0300

    feat: Create exception eror is not authenticated

--- contratos/views.py
+++ contratos/views.py
@@ -1,5 +1,6 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
 from rest_framework.parsers import FileUploadParser
+from rest_framework.exceptions import NotFound
 from .models import Contrato
 from .serializer import ContratoSerializer
 
@@ -12,3 +13,15 @@ class ContratoViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(usuario=self.request.user)
 
+    def get_object(self):
+        try:
+            obj = super().get_object()
+            self.check_object_permissions(self.request, obj)
+            return obj
+        except Contrato.DoesNotExist:
+            raise NotFound()
+
+    def create(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            raise NotFound()
+        return super().create(request, *args, **kwargs)
